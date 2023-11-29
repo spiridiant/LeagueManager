49f7a85a533f02be22e53be233da2986aae3ad48
Susan <susanchung-brave@users.noreply.github.com>
2023-11-29 11:25:04 -0800

    select is more user friendly - toggles comparison display depending on attribute selected - no longer case sensitive! - general clean up

--- src/main/database/DatabaseConnectionHandler.java
+++ src/main/database/DatabaseConnectionHandler.java
@@ -378,7 +378,13 @@ public class DatabaseConnectionHandler {
         try {
             String query = "SELECT * FROM Team WHERE ";
 
-            query += attribute + " " + comparison + " ?";
+            if (comparison.equals("LIKE")) {
+                query += "UPPER(" + attribute + ") "  + "LIKE" + " UPPER(?)";
+            } else if (attribute.equals("Cap_Space")) {
+                query += attribute + " " + comparison + " ?";
+            } else if (comparison.equals("=")) { // string equals
+                query += "UPPER(" + attribute + ") "  + comparison + " UPPER(?)";
+            }
 
             PrintablePreparedStatement ps = new PrintablePreparedStatement(connection.prepareStatement(query), query, false);
 
