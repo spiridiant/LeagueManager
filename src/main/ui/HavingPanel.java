b65e57e65775bd3a1f1ef131f3a4d57ed4c3050d
UBC Student <kookevk@students.cs.ubc.ca>
2023-11-29 17:00:33 -0800

    Update HavingPanel.java
    adjusted backgroundcolor

--- src/main/ui/HavingPanel.java
+++ src/main/ui/HavingPanel.java
@@ -20,7 +20,7 @@ public class HavingPanel extends JPanel {
 
     public HavingPanel(CardLayout cl, JPanel leagueManager, TerminalOperationDelegate delegate) {
         this.setPreferredSize(new Dimension(PANEL_WIDTH, PANEL_HEIGHT));
-        this.setBackground(new Color(34, 34, 34));
+        this.setBackground(new Color(22, 30, 51));
 
         this.delegate = delegate;
         this.cl = cl;
